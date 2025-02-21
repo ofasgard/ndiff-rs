4783538de6e000c3cdeda4422d9a9e9d6b3d3af0
ofasgard <19550999+ofasgard@users.noreply.github.com>
2025-02-21 13:19:41 +0000

    code restructure to group trait implementations

--- src/host.rs
+++ src/host.rs
@@ -32,113 +32,6 @@ impl HostWrapper {
 	}
 }
 
-impl PartialEq for HostWrapper {
-	fn eq(&self, other: &HostWrapper) -> bool {
-		// If any of the IP or MAC addresses match between two hosts, we consider them to be the same host.
-		for self_address in self.0.addresses() {
-			for other_address in other.0.addresses() {
-				if self_address == other_address { return true; }
-			}
-		}
-		
-		false
-	}
-}
-
-impl fmt::Display for HostWrapper {
-	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-		write!(f, "Status: {} ({})\n\n", self.0.status.state.to_string(), self.0.status.reason)?;
-		
-		let portinfo = PortInfoWrapper(self.0.port_info.clone());
-		write!(f, "Ports:\n\n{}\n", portinfo.to_string())?;
-		
-		let addresses = AddressesWrapper(self.0.addresses().map(|x| x.clone()).collect());
-		write!(f, "Addresses:\n\n{}\n", addresses.to_string())?;
-		
-		let hostnames = HostnamesWrapper(self.0.host_names().map(|x| x.clone()).collect());
-		write!(f, "Hostnames:\n\n{}\n", hostnames.to_string())?;
-		
-		Ok(())
-	}
-}
-
-impl PartialEq for HostStatusWrapper {
-	fn eq(&self, other: &HostStatusWrapper) -> bool {
-		// If the state and reason is the same, we don't consider host state to have changed.
-		self.0.state == other.0.state && self.0.reason == other.0.reason
-	}
-}
-
-impl PartialEq for PortInfoWrapper {
-	fn eq(&self, other: &PortInfoWrapper) -> bool {
-		// Convert both iterators into a vector of ports.
-		let mut left : Vec<&Port> = self.0.ports().collect();
-		let mut right : Vec<&Port> = other.0.ports().collect();
-		
-		// Sort and compare the vectors to determine whether the port list is identical.
-		left.sort_by_key(|x| { x.port_number });
-		right.sort_by_key(|x| { x.port_number });
-		left == right
-	}
-}
-
-impl fmt::Display for PortInfoWrapper {
-	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-		for port in self.0.ports() {
-			write!(f, "{} {} ({})", port.protocol.to_string(), port.port_number, port.status.reason)?;
-			if let Some(serviceinfo) = &port.service_info {
-				write!(f, " [{}]", serviceinfo.name)?;
-			}
-			write!(f, "\n")?;
-		}
-		Ok(())
-	}
-}
-
-impl PartialEq for AddressesWrapper {
-	fn eq(&self, other: &AddressesWrapper) -> bool {
-		// Compare the addresses, irrespective of order, for equality.
-		let mut eq = true;
-		for address in &self.0 {
-			if !other.0.contains(&address) { eq = false; }
-		}
-		eq
-	}
-}
-
-impl fmt::Display for AddressesWrapper {
-	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-		for address in &self.0 {
-			let addr_str = match address {
-				Address::IpAddr(x) => x.to_string(),
-				Address::MacAddr(x) => x.to_string()
-			};
-			write!(f, "{}\n", addr_str)?;
-		}
-		Ok(())
-	}
-}
-
-impl PartialEq for HostnamesWrapper {
-	fn eq(&self, other: &HostnamesWrapper) -> bool {
-		// Compare the hostnames, irrespective of order, for equality.
-		let mut eq = true;
-		for hostname in &self.0 {
-			if !other.0.contains(&hostname) { eq = false; }
-		}
-		eq
-	}
-}
-
-impl fmt::Display for HostnamesWrapper {
-	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-		for hostname in &self.0 {
-			write!(f, "{}\n", hostname.name)?;
-		}
-		Ok(())
-	}
-}
-
 #[derive(Debug)]
 pub struct HostDiff {
 	pub title: String,
@@ -182,37 +75,6 @@ impl HostDiff {
 			addresses: addresses,
 			hostnames: hostnames
 		}
-	}
-}
-
-impl fmt::Display for HostDiff {
-	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-		if let Some(status) = &self.status {
-			write!(f, "Status: {} ({}) => {} ({})\n\n", status.0.state.to_string(), status.0.reason, status.1.state.to_string(), status.1.reason)?;
-		}
-		
-		if let Some(portinfo) = &self.portinfo {
-			let left = PortInfoWrapper(portinfo.0.clone());
-			let right = PortInfoWrapper(portinfo.1.clone());
-			write!(f, "Old Ports:\n\n{}\n", left.to_string())?;
-			write!(f, "New Ports:\n\n{}\n", right.to_string())?;
-		}
-		
-		if let Some(addresses) = &self.addresses {
-			let left = AddressesWrapper(addresses.0.clone());
-			let right = AddressesWrapper(addresses.1.clone());
-			write!(f, "Old Addresses:\n\n{}\n", left.to_string())?;
-			write!(f, "New Addresses:\n\n{}\n", right.to_string())?;
-		}
-		
-		if let Some(hostnames) = &self.hostnames {
-			let left = HostnamesWrapper(hostnames.0.clone());
-			let right = HostnamesWrapper(hostnames.1.clone());
-			write!(f, "Old Hostnames:\n\n{}\n", left.to_string())?;
-			write!(f, "New Hostnames:\n\n{}\n", right.to_string())?;
-		}
-		
-		Ok(())
 	}
 }
 
@@ -260,6 +122,148 @@ impl HostDelta {
 		}
 	
 		output
+	}
+}
+
+// EQUALITY IMPLEMENTATIONS
+
+impl PartialEq for HostWrapper {
+	fn eq(&self, other: &HostWrapper) -> bool {
+		// If any of the IP or MAC addresses match between two hosts, we consider them to be the same host.
+		for self_address in self.0.addresses() {
+			for other_address in other.0.addresses() {
+				if self_address == other_address { return true; }
+			}
+		}
+		
+		false
+	}
+}
+
+impl PartialEq for HostStatusWrapper {
+	fn eq(&self, other: &HostStatusWrapper) -> bool {
+		// If the state and reason is the same, we don't consider host state to have changed.
+		self.0.state == other.0.state && self.0.reason == other.0.reason
+	}
+}
+
+impl PartialEq for PortInfoWrapper {
+	fn eq(&self, other: &PortInfoWrapper) -> bool {
+		// Convert both iterators into a vector of ports.
+		let mut left : Vec<&Port> = self.0.ports().collect();
+		let mut right : Vec<&Port> = other.0.ports().collect();
+		
+		// Sort and compare the vectors to determine whether the port list is identical.
+		left.sort_by_key(|x| { x.port_number });
+		right.sort_by_key(|x| { x.port_number });
+		left == right
+	}
+}
+
+impl PartialEq for AddressesWrapper {
+	fn eq(&self, other: &AddressesWrapper) -> bool {
+		// Compare the addresses, irrespective of order, for equality.
+		let mut eq = true;
+		for address in &self.0 {
+			if !other.0.contains(&address) { eq = false; }
+		}
+		eq
+	}
+}
+
+impl PartialEq for HostnamesWrapper {
+	fn eq(&self, other: &HostnamesWrapper) -> bool {
+		// Compare the hostnames, irrespective of order, for equality.
+		let mut eq = true;
+		for hostname in &self.0 {
+			if !other.0.contains(&hostname) { eq = false; }
+		}
+		eq
+	}
+}
+
+// DISPLAY IMPLEMENTATIONS
+
+impl fmt::Display for HostWrapper {
+	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+		write!(f, "Status: {} ({})\n\n", self.0.status.state.to_string(), self.0.status.reason)?;
+		
+		let portinfo = PortInfoWrapper(self.0.port_info.clone());
+		write!(f, "Ports:\n\n{}\n", portinfo.to_string())?;
+		
+		let addresses = AddressesWrapper(self.0.addresses().map(|x| x.clone()).collect());
+		write!(f, "Addresses:\n\n{}\n", addresses.to_string())?;
+		
+		let hostnames = HostnamesWrapper(self.0.host_names().map(|x| x.clone()).collect());
+		write!(f, "Hostnames:\n\n{}\n", hostnames.to_string())?;
+		
+		Ok(())
+	}
+}
+
+impl fmt::Display for PortInfoWrapper {
+	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+		for port in self.0.ports() {
+			write!(f, "{} {} ({})", port.protocol.to_string(), port.port_number, port.status.reason)?;
+			if let Some(serviceinfo) = &port.service_info {
+				write!(f, " [{}]", serviceinfo.name)?;
+			}
+			write!(f, "\n")?;
+		}
+		Ok(())
+	}
+}
+
+impl fmt::Display for AddressesWrapper {
+	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+		for address in &self.0 {
+			let addr_str = match address {
+				Address::IpAddr(x) => x.to_string(),
+				Address::MacAddr(x) => x.to_string()
+			};
+			write!(f, "{}\n", addr_str)?;
+		}
+		Ok(())
+	}
+}
+
+impl fmt::Display for HostnamesWrapper {
+	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+		for hostname in &self.0 {
+			write!(f, "{}\n", hostname.name)?;
+		}
+		Ok(())
+	}
+}
+
+impl fmt::Display for HostDiff {
+	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+		if let Some(status) = &self.status {
+			write!(f, "Status: {} ({}) => {} ({})\n\n", status.0.state.to_string(), status.0.reason, status.1.state.to_string(), status.1.reason)?;
+		}
+		
+		if let Some(portinfo) = &self.portinfo {
+			let left = PortInfoWrapper(portinfo.0.clone());
+			let right = PortInfoWrapper(portinfo.1.clone());
+			write!(f, "Old Ports:\n\n{}\n", left.to_string())?;
+			write!(f, "New Ports:\n\n{}\n", right.to_string())?;
+		}
+		
+		if let Some(addresses) = &self.addresses {
+			let left = AddressesWrapper(addresses.0.clone());
+			let right = AddressesWrapper(addresses.1.clone());
+			write!(f, "Old Addresses:\n\n{}\n", left.to_string())?;
+			write!(f, "New Addresses:\n\n{}\n", right.to_string())?;
+		}
+		
+		if let Some(hostnames) = &self.hostnames {
+			let left = HostnamesWrapper(hostnames.0.clone());
+			let right = HostnamesWrapper(hostnames.1.clone());
+			write!(f, "Old Hostnames:\n\n{}\n", left.to_string())?;
+			write!(f, "New Hostnames:\n\n{}\n", right.to_string())?;
+		}
+		
+		Ok(())
 	}
 }
 
