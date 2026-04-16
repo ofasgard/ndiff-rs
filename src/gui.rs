b633fb0e82417bd0df831d491fc52c75f4abf7e7
Callum Murphy-Hale <19550999+ofasgard@users.noreply.github.com>
2026-04-16 23:02:08 +0100

    Add logic for loading a processing scans

--- src/gui.rs
+++ src/gui.rs
@@ -1,9 +1,15 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
+use std::fs;
 use eframe::egui;
 use egui::Layout;
 use egui::Align;
+use egui::containers::ComboBox;
 use egui::widgets::Separator;
+use rfd::FileDialog;
+
+use crate::host::HostDelta;
+use nmap_xml_parser::NmapResults;
 
 pub fn run_gui() -> eframe::Result {
 	let mut options = eframe::NativeOptions::default();
@@ -17,15 +23,19 @@ pub fn run_gui() -> eframe::Result {
 }
 
 struct NDiffApp {
-			first_scan: Option<String>,
-			second_scan: Option<String>
+			left_scan: Option<NmapResults>,
+			right_scan: Option<NmapResults>,
+			deltas: Vec<HostDelta>,
+			processed: bool
 }
 
 impl Default for NDiffApp {
 	fn default() -> Self {
 		Self {
-			first_scan: None,
-			second_scan: None
+			left_scan: None,
+			right_scan: None,
+			deltas: Vec::new(),
+			processed: false
 		}
 	}
 }
@@ -33,27 +43,71 @@ impl Default for NDiffApp {
 impl eframe::App for NDiffApp {
 	fn ui(&mut self, ui: &mut egui::Ui, _frame: &mut eframe::Frame) {
 		egui::CentralPanel::default().show_inside(ui, |ui| {
-			ui.label("Drag and drop two Nmap XML scans into this window!");
+			ui.label("Load two Nmap XML scans!");
 			ui.add(Separator::default().spacing(32.0));
 			
 			ui.with_layout(Layout::left_to_right(Align::TOP), |ui| {
 				ui.with_layout(Layout::top_down(Align::TOP), |ui| {
 					ui.heading("First Scan");
 					ui.add_space(32.0);
-					match &self.first_scan {
-						Some(scan) => todo!(),
-						None => ui.code_editor(&mut "Waiting for a scan...")
-					}
+					match &self.left_scan {
+						Some(_) => { ui.code_editor(&mut "Scan loaded!"); },
+						None => { 
+							ui.code_editor(&mut "Waiting for a scan...");
+							if ui.button("Open file...").clicked() && let Some(path) = FileDialog::new().pick_file() {
+								let path_str = path.display().to_string();
+								self.left_scan = self.load_scan(path_str, ui);
+							}
+						}
+					};
+					if self.processed { self.render_deltas(); }
 				});
+				
 				ui.with_layout(Layout::top_down(Align::TOP), |ui| {
 					ui.heading("Second Scan");
 					ui.add_space(32.0);
-					match &self.second_scan {
-						Some(scan) => todo!(),
-						None => ui.code_editor(&mut "Waiting for a scan...")
-					}
+					match &self.right_scan {
+						Some(_) => { ui.code_editor(&mut "Scan loaded!"); },
+						None => { 
+							ui.code_editor(&mut "Waiting for a scan...");
+							if ui.button("Open file...").clicked() && let Some(path) = FileDialog::new().pick_file() {
+								let path_str = path.display().to_string();
+								self.right_scan = self.load_scan(path_str, ui);
+							}
+						}
+					};
+					if self.processed { self.render_deltas(); }
 				});
 			});
+			
+			if self.left_scan.is_some() && self.right_scan.is_some() && !self.processed {
+				self.deltas = HostDelta::from_scans(&self.left_scan.clone().unwrap(), &self.right_scan.clone().unwrap());
+				self.processed = true;
+			}
 		});
 	}
 }
+
+impl NDiffApp {
+	fn load_scan(&mut self, path: String, ui: &mut egui::Ui) -> Option<NmapResults> {
+		let content = match fs::read_to_string(path) {
+			Ok(x) => x,
+			Err(e) => {
+				return None; // TODO display some error
+			}
+		};
+		
+		match NmapResults::parse(&content) {
+			Ok(x) => Some(x),
+			Err(e) => {
+				None // TODO display some error
+			}
+		}
+	}
+	
+	fn render_deltas(&mut self) {
+		todo!();
+	}
+}
+
+
